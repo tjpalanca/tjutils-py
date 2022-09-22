0205940407a9ef989f76026efd7d29df39251aa5
TJ P <mail@tjpalanca.com>
2022-09-22 16:50:10 +0000

    nbdev export

--- tjutils/_modidx.py
+++ tjutils/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://tjpalanca.github.io',
                 'git_url': 'https://github.com/tjpalanca/tjutils-py',
                 'lib_path': 'tjutils'},
-  'syms': {'tjutils.nbdev': {'tjutils.nbdev.sync': ('01-nbdev.html#sync', 'tjutils/nbdev.py')}}}
+  'syms': {'tjutils.nbdev': {'tjutils.nbdev.export': ('01-nbdev.html#export', 'tjutils/nbdev.py')}}}
